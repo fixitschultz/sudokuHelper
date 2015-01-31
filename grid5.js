897a3a6a845fb6c04ee787fac27d8b461a7df553
fixitschultz <matthewleeschultz@gmail.com>
2015-01-30 23:43:36 -0500

    Update grid5.js

--- grid5.js
+++ grid5.js
@@ -1,11 +1,34 @@
-<script>
-function rcToNum(var rowf,  var colf) {
+function grabBoard(){
+var board= new Array(100);
+var targeturl;
+var spot=0;
+for(var x=11; x<100; x++){
+var id="b";
+
+	if( x%10==0){
+		x++;
+	}
+	id+=x;
+	boardValue=document.getElementById(id).value;
+
+	if(boardValue === ""){
+	board[spot++]=0;
+	}else{
+	board[spot++]=boardValue;
+	}
+}
+return board;
+}
+
+function rcToNum(rowf, colf) {
 	return (rowf*10)+colf;
 }
+
 function gridPrintout(){
 	var grid=" ";
-	var board="000000000000000000000000000000000000000000000000000000000000000000000000000000009";
+	var board=grabBoard();
 	var rx=0;
+	var newBoard= new Array(100);
 	for(var b=11; b<100; b++){
 		if(b%10==0){
 			b++;
@@ -13,13 +36,12 @@ function gridPrintout(){
 			newBoard[b]=board[rx];
 			rx++;
 	}
-			grid+= '<table border="1" cellpading="1">';
+	/*
+		printing sudoku grid
+	*/
 	grid+= '<tr>';
 	for (var mx = 0; mx < 81; mx++) {
-		if(board[mx]==0)
-		{
-			board[mx]=" ";
-		}
+		
 		if((mx+1)%9==0)
 		{
 		grid+= '<td>'+board[mx]+'</td>';
@@ -28,12 +50,18 @@ function gridPrintout(){
 			grid+= '<td>'+board[mx]+'</td>';
 		}
 	}
-	grid+= '</table>';
+	
+	document.getElementById("puzzle").innerHTML=grid;
+	/*
+		end of printing sudoku grid
+	*/
 	var boardHelper = new Array(100);
 	  for (var ih = 0; ih < 100; ih++) {
 		boardHelper[ih] = new Array(10);
 	 }
-	//boardHelper[0][0]=0;
+	 for( var mr=0; mr < 100; mr++){
+		boardHelper[mr][0]=0;
+	 }
 	var rc=0;
 	var col=0;
 	var row=0;
@@ -52,11 +80,17 @@ function gridPrintout(){
 		}
 		if((newBoard[ax]>0)&&(newBoard[ax]<10)){
 			num=newBoard[ax];
-			row=substr(ax,0,1);
-			col=substr(ax,1,1);
+			console.log("ax "+ax);
+			col=ax%10;
+			row=(ax-(ax%10))/10;
+			console.log("row "+row+" col "+col);
 			rc=rcToNum(row,col); 
-			boardHelper[rc][num]=1; /* clear row of the number*/
+			console.log("rc "+rc);
+			boardHelper[rc][num]=1; 
+			boardHelper[rc][0]=9;
 			/* clear the rest of the rc cells*/
+			
+			/* clear row of the number*/
 			for(var currentX=1; currentX<10; currentX++){
 				if(boardHelper[rc][currentX]!=1){
 					boardHelper[rc][currentX]= -1;
@@ -65,16 +99,23 @@ function gridPrintout(){
 			/* clear row of the number*/
 			for(var rowX=1; rowX<10; rowX++){
 				if(rowX!=col){
-					if(boardHelper[rcToNum(row,rowX)][num]!=1){
+					if((boardHelper[rcToNum(row,rowX)][num]!==1)&&(boardHelper[rcToNum(row,rowX)][num]!==-1)){
 						boardHelper[rcToNum(row,rowX)][num]= -1;
+						if(boardHelper[rcToNum(row,rowX)][0]<=7){
+						boardHelper[rcToNum(row,rowX)][0]=boardHelper[rcToNum(row,rowX)][0]+1;
+						}
 					}
 				}
 			}
 			// clear column of the number 
 			for(var colX=0; colX<10; colX++){ 
 				if((rcToNum(colX,col)!==rc)&&(rcToNum(colX,col)>10)){
-					if(boardHelper[rcToNum(colX,col)][num]!=1){
+					if((boardHelper[rcToNum(colX,col)][num]!==1)&&(boardHelper[rcToNum(colX,col)][num]!==-1)){
 						boardHelper[rcToNum(colX,col)][num]=-1;
+						if(boardHelper[rcToNum(colX,col)][0]<=7){
+							boardHelper[rcToNum(colX,col)][0]=boardHelper[rcToNum(colX,col)][0]+1;
+						}
+						
 					}
 					//grid+="row "+row+" colX "+colX;
 				}
@@ -107,65 +148,95 @@ function gridPrintout(){
 				for(var spotC=0; spotC<3; spotC++)
 				{
 					if((rcToNum(((row-topR)+spotR),((col-topC)+spotC)))!==rc){
-					if(boardHelper[rcToNum(((row-topR)+spotR),((col-topC)+spotC))][num]!==-1){
+					if((boardHelper[rcToNum(((row-topR)+spotR),((col-topC)+spotC))][num]!==1)&&((boardHelper[rcToNum(((row-topR)+spotR),((col-topC)+spotC))][num]!==-1))){
 						boardHelper[rcToNum(((row-topR)+spotR),((col-topC)+spotC))][num]=-1;
+						
+						if(boardHelper[rcToNum(((row-topR)+spotR),((col-topC)+spotC))][0]!=9){
+							boardHelper[rcToNum(((row-topR)+spotR),((col-topC)+spotC))][0]=boardHelper[rcToNum(((row-topR)+spotR),((col-topC)+spotC))][0]+1;
+						}
+						
 					}
 				}
 				}
 			}
 		}
 	}
-
+	
 	for(var count=1; count<10; count++){
-		grid+= "<br/>\n<br/>\n<br/>\n";
-		grid+= '<table align="center" border="1" cellpading="3">';
-		grid+= ' <tr> <td width="45">Row '+(count)+'</td><td width="25">1</td><td width="25">2</td><td width="25">3</td><td width="25">4</td><td width="25">5</td><td width="25">6</td><td width="25">7</td><td width="25">8</td><td width="25">9</td></tr>';
+		var rowColor="white";
+		grid="";
+		grid+= ' <tr><td width="25"> </td><td width="45">Row '+(count)+'</td><td width="25">1</td><td width="25">2</td><td width="25">3</td><td width="25">4</td><td width="25">5</td><td width="25">6</td><td width="25">7</td><td width="25">8</td><td width="25">9</td></tr>';
 		grid+= '<tr>';
 		for(var ar=1; ar<10; ar++){
 			if(ar!=1){
 				grid+='</tr><tr>';
 			}
-			grid+=' <td width="25">'+rcToNum((count),(a))+'</td>';
+			if(boardHelper[rcToNum((count),(ar))][0]===8)
+				{
+					rowColor="pink";
+				}else{
+					rowColor="white";
+				}
+			grid+='<td width="25" bgcolor='+rowColor+'>'+boardHelper[rcToNum((count),(ar))][0]+'</td><td width="25" bgcolor='+rowColor+'>'+rcToNum((count),(ar))+'</td>';
 			for(var rv=1; rv<10; rv++){
 				if( boardHelper[rcToNum((count),(ar))][rv] ==1){
-				grid+= '<td width="25">@</td>';
+				grid+= '<td width="25" bgcolor="'+rowColor+'">@</td>';
 				}else if(boardHelper[rcToNum((count),(ar))][rv]== (-1)){
-					grid+= '<td width="25">X</td>';
+					grid+= '<td width="25" bgcolor="'+rowColor+'">X</td>';
 				 }else{
-					   grid+= '<td width="25"> </td>';
+					   grid+= '<td width="25" bgcolor="'+rowColor+'"> </td>';
 				}
 			}
 		}
-					grid+= '</table>';
+		document.getElementById("row"+count).innerHTML=grid;			
 	}
+	
+	/* end of printing row grids */
+	
+	/* printing column grids */
+	
 	for(var count2=1; count2<10; count2++){
-		grid+= "<br/>\n<br/>\n<br/>\n";
-		grid+= '<table align="center" border="1" cellpading="3">';
-		grid+= ' <tr> <td width="65">Column '+(count2)+'</td><td width="25">1</td><td width="25">2</td><td width="25">3</td><td width="25">4</td><td width="25">5</td><td width="25">6</td><td width="25">7</td><td width="25">8</td><td width="25">9</td></tr>';
+		grid="";
+		
+		var colColor="white";
+			
+		grid+= ' <tr><td width="25"> </td><td width="65">Column '+(count2)+'</td><td width="25">1</td><td width="25">2</td><td width="25">3</td><td width="25">4</td><td width="25">5</td><td width="25">6</td><td width="25">7</td><td width="25">8</td><td width="25">9</td></tr>';
 		grid+= '<tr>';
 		for(var at=1; at<10; at++){
 			if(at!=1){
 			grid+='</tr><tr>';
 			}
-			grid+=' <td width="25">'+rcToNum(at,count2)+'</td>';
+			if(boardHelper[rcToNum(at,count2)][0]===8)
+			{
+				colColor="pink";
+			}else{
+				colColor="white";
+			}
+			
+			grid+='<td width="25" bgcolor="'+colColor+'">'+boardHelper[rcToNum(at,count2)][0]+'</td><td width="25" bgcolor="'+colColor+'">'+rcToNum(at,count2)+'</td>';
 			for(var cv=1; cv<10; cv++){
 					switch( boardHelper[rcToNum(at,count2)][cv])
 					{
 						case (-1):
-							grid+= '<td width="25">X</td>';
+							grid+= '<td width="25" bgcolor="'+colColor+'">X</td>';
 							break;
 						case 1:
-							grid+= '<td width="25">@</td>';
+							grid+= '<td width="25" bgcolor="'+colColor+'">@</td>';
 							break;
 						default :
-							grid+= '<td width="25"> </td>';
+							grid+= '<td width="25" bgcolor="'+colColor+'"> </td>';
 					}
 				}
 		}
-					grid+= '</table>';
+					document.getElementById("col"+count2).innerHTML=grid;
 	}
-
+	
+	/* end of print of column grids */
+	/* printing of box grids */
 	for(var count3=0; count3<9; count3++){
+		var boxColor="white";
+		grid="";
+		
 	switch(count3)
 	{
 		case 0:
@@ -208,33 +279,32 @@ function gridPrintout(){
 		   tr=0;
 		   tc=0;
 	}
-	grid+= "<br/>\n<br/>\n<br/>\n";
-	grid+= '<table align="center" border="1" cellpading="3">';
-	grid+= ' <tr> <td width="45">box '+(count3+1)+'</td><td width="25">1</td><td width="25">2</td><td width="25">3</td><td width="25">4</td><td width="25">5</td><td width="25">6</td><td width="25">7</td><td width="25">8</td><td width="25">9</td></tr>';
+	grid+= ' <tr><td width="25"> </td><td width="45">box '+(count3+1)+'</td><td width="25">1</td><td width="25">2</td><td width="25">3</td><td width="25">4</td><td width="25">5</td><td width="25">6</td><td width="25">7</td><td width="25">8</td><td width="25">9</td></tr>';
 	grid+= '<tr>';
 		for(var ya=0; ya<3; ya++){
 		   for(var fb=0; fb<3; fb++){
-		   if(fb==1){
-			  grid+=' <td width="25">'+rcToNum((tr+ya),(tc+fb))+'</td>';
-			  }else{
-			  grid+='</tr><tr> <td width="25">'+rcToNum((tr+ya),(tc+fb))+'</td>';
-			  }
+		   if(boardHelper[rcToNum((tr+ya),(tc+fb))][0]===8)
+			{
+				boxColor="pink";
+			}else{
+				boxColor="white";
+			}
+			  grid+='</tr><tr><td width="25" bgcolor="'+boxColor+'">'+boardHelper[rcToNum((tr+ya),(tc+fb))][0]+'</td><td width="25" bgcolor="'+boxColor+'">'+rcToNum((tr+ya),(tc+fb))+'</td>';
 			for(var ev=1; ev<10; ev++){
 				switch( boardHelper[rcToNum((tr+ya),(tc+fb))][ev])
 				{
 					case (-1):
-					grid+= '<td width="25">X</td>';
+					grid+= '<td width="25" bgcolor="'+boxColor+'">X</td>';
 					break;
 					case 1:
-					grid+= '<td width="25">@</td>';
+					grid+= '<td width="25" bgcolor="'+boxColor+'">@</td>';
 					break;
 					default :
-					grid+= '<td width="25"> </td>';
+					grid+= '<td width="25" bgcolor="'+boxColor+'"> </td>';
 				}
 			}
 			}
 		}
-				grid+= '</table>';
+				document.getElementById("box"+(count3+1)).innerHTML=grid;
 	}
-}
-?>
+	}
